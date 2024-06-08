e4eae1c0219824e04da851cab20f19bd7de0ecf1
WeBstwr <ifedhawebster@gmail.com>
2024-06-08 23:36:35 +0300

    create the basic structure of contact page
    I have created the basic structure of the contact form.

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,67 @@
+import "./contact.css";
+import logo from "../../assets/logo.png";
+
 const Contact = () => {
-  return <h1>contatsttt</h1>;
+  return (
+    <>
+      <div className="contact-background">
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+      </div>
+
+      <section className="contact-section">
+        <div className="contact-info">
+          <div className="contacts">
+            <h2>Get in Touch</h2>
+          </div>
+          <div className="contact-form-sectiom">
+            <form className="contact-form">
+              <div className="contact-form-grp">
+                <label for="name">name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  className="contact-form-input"
+                  placeholder="enter your name"
+                />
+              </div>
+              <div className="contact-form-grp">
+                <label for="email">email:</label>
+                <input
+                  type="text"
+                  id="email"
+                  className="contact-form-input"
+                  placeholder="enter email address"
+                />
+              </div>
+              <div className="contact-form-grp">
+                <label for="name">Name:</label>
+                <textarea
+                  id="message"
+                  className="contact-form-input"
+                ></textarea>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="maps">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6708.300292238894!2d36.8176032918446!3d-1.3076741621547114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1172d84d49a7%3A0xf7cf0254b297924c!2sNairobi!5e0!3m2!1sen!2ske!4v1717876071933!5m2!1sen!2ske"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            loading="lazy"
+          ></iframe>
+          <div className="open-hours">
+            <h2>Open Hours</h2>
+            <h2>Open Hours</h2>
+            <h2>Open Hours</h2>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
--- src/pages/TripTypes/TripTypes.jsx
+++ src/pages/TripTypes/TripTypes.jsx
@@ -1,5 +1,192 @@
+import "./tripTypes.css";
+import honeymoon from "../../assets/honeymoon.jpg";
+
 const TripTypes = () => {
-  return <></>;
+  return (
+    <>
+      <div className="trip-background">
+        <div className="trip-bg-text">
+          <h2>trip types</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque
+            rerum iure inventore esse, ab officia!
+          </p>
+        </div>
+      </div>
+
+      <section className="trip-types">
+        <div className="trip-container">
+          <div className="trip-title">
+            <h3>honeymoon</h3>
+          </div>
+          <div className="trip-image">
+            <img src={honeymoon} alt="Honeymoon" />
+          </div>
+          <div className="trip-text-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              veniam.
+            </p>
+            <h4>$5000</h4>
+            <button>book</button>
+          </div>
+        </div>
+
+        <div className="trip-container">
+          <div className="trip-title">
+            <h3>family</h3>
+          </div>
+          <div className="trip-image">
+            <img src={honeymoon} alt="Honeymoon" />
+          </div>
+          <div className="trip-text-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              veniam.
+            </p>
+            <h4>$5000</h4>
+            <button>book</button>
+          </div>
+        </div>
+
+        <div className="trip-container">
+          <div className="trip-title">
+            <h3>adventure</h3>
+          </div>
+          <div className="trip-image">
+            <img src={honeymoon} alt="Honeymoon" />
+          </div>
+          <div className="trip-text-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              veniam.
+            </p>
+            <h4>$5000</h4>
+            <button>book</button>
+          </div>
+        </div>
+
+        <div className="trip-container">
+          <div className="trip-title">
+            <h3>cultural tours</h3>
+          </div>
+          <div className="trip-image">
+            <img src={honeymoon} alt="Honeymoon" />
+          </div>
+          <div className="trip-text-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              veniam.
+            </p>
+            <h4>$5000</h4>
+            <button>book</button>
+          </div>
+        </div>
+
+        <div className="trip-container">
+          <div className="trip-title">
+            <h3>wildlife safaris</h3>
+          </div>
+          <div className="trip-image">
+            <img src={honeymoon} alt="Honeymoon" />
+          </div>
+          <div className="trip-text-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              veniam.
+            </p>
+            <h4>$5000</h4>
+            <button>book</button>
+          </div>
+        </div>
+
+        <div className="trip-container">
+          <div className="trip-title">
+            <h3>eco-tours</h3>
+          </div>
+          <div className="trip-image">
+            <img src={honeymoon} alt="Honeymoon" />
+          </div>
+          <div className="trip-text-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              veniam.
+            </p>
+            <h4>$5000</h4>
+            <button>book</button>
+          </div>
+        </div>
+
+        <div className="trip-container">
+          <div className="trip-title">
+            <h3>luxury vacations</h3>
+          </div>
+          <div className="trip-image">
+            <img src={honeymoon} alt="Honeymoon" />
+          </div>
+          <div className="trip-text-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              veniam.
+            </p>
+            <h4>$5000</h4>
+            <button>book</button>
+          </div>
+        </div>
+
+        <div className="trip-container">
+          <div className="trip-title">
+            <h3>beach holidays</h3>
+          </div>
+          <div className="trip-image">
+            <img src={honeymoon} alt="Honeymoon" />
+          </div>
+          <div className="trip-text-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              veniam.
+            </p>
+            <h4>$5000</h4>
+            <button>book</button>
+          </div>
+        </div>
+
+        <div className="trip-container">
+          <div className="trip-title">
+            <h3>wellness retreats</h3>
+          </div>
+          <div className="trip-image">
+            <img src={honeymoon} alt="Honeymoon" />
+          </div>
+          <div className="trip-text-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              veniam.
+            </p>
+            <h4>$5000</h4>
+            <button>book</button>
+          </div>
+        </div>
+
+        <div className="trip-container">
+          <div className="trip-title">
+            <h3>culnary vacations</h3>
+          </div>
+          <div className="trip-image">
+            <img src={honeymoon} alt="Honeymoon" />
+          </div>
+          <div className="trip-text-box">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+              veniam.
+            </p>
+            <h4>$5000</h4>
+            <button>book</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default TripTypes;
